c11d095c5a87fef511ad82ba099b4c820533094d
ifaamrillah <amrillah.work@gmail.com>
2025-07-22 13:37:06 +0700

    feat: add welcome endpoint

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+import { ENV } from "./config/env.config";
+import { HTTPSTATUS } from "./config/http.config";
+
+const app = express();
+const BASE_PATH = ENV.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ENV.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(HTTPSTATUS.OK).json({
+    message: "Welcome to CashMind API",
+  });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+});
