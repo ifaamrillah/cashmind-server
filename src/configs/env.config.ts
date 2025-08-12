bb9cfa1fc4eb27709de6a43d2d36c674710268bc
ifaamrillah <amrillah.work@gmail.com>
2025-08-12 11:36:19 +0700

    feat: add env config

---
+++ src/configs/env.config.ts
@@ -0,0 +1,22 @@
+import { getEnv } from "../utils/get-env";
+
+const envConfig = () => ({
+  PORT: getEnv("PORT", "8000"),
+  NODE_ENV: getEnv("NODE_ENV", "development"),
+  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "http://localhost:3000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api"),
+
+  MONGO_URI: getEnv("MONGO_URI"),
+
+  JWT_SECRET: getEnv("JWT_SECRET", "jwt_secret"),
+  JWT_EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "1d"),
+
+  JWT_REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET", "jwt_refresh_secret"),
+  JWT_REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_EXPIRES_IN", "7d"),
+
+  GEMINI_API_KEY: getEnv("GEMINI_API_KEY"),
+
+  CRON_SECRET: getEnv("CRON_SECRET", "cron_secret"),
+});
+
+export const ENV = envConfig();
