6fad689e40e1811e5a40c05a7de229eb442daea7
ifaamrillah <amrillah.work@gmail.com>
2025-08-16 18:26:31 +0700

    feat: add login service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,10 +5,14 @@ import ReportSettingModel, {
   ReportFrequencyEnum,
 } from "../models/report-setting.model";
 
-import { RegisterSchemaType } from "../validators/auth.validator";
+import {
+  LoginSchemaType,
+  RegisterSchemaType,
+} from "../validators/auth.validator";
 
-import { UnauthorizedException } from "../utils/app-error";
+import { NotFoundException, UnauthorizedException } from "../utils/app-error";
 import { calculateNextReportDate } from "../utils/helper";
+import { signJwtToken } from "../utils/jwt";
 
 export const registerService = async (body: RegisterSchemaType) => {
   const { name, email, password } = body;
@@ -16,7 +20,7 @@ export const registerService = async (body: RegisterSchemaType) => {
   const session = await mongoose.startSession();
 
   try {
-    const user = await session.withTransaction(async () => {
+    const result = await session.withTransaction(async () => {
       const existingUser = await UserModel.findOne({ email }).session(session);
       if (existingUser) {
         throw new UnauthorizedException("User already exists");
@@ -38,13 +42,51 @@ export const registerService = async (body: RegisterSchemaType) => {
 
       await reportSetting.save({ session });
 
-      return newUser.omitPassword();
+      return {
+        user: newUser.omitPassword(),
+      };
     });
 
-    return user;
+    return result;
   } catch (error) {
     throw error;
   } finally {
     await session.endSession();
   }
+};
+
+export const loginService = async (body: LoginSchemaType) => {
+  const { email, password } = body;
+
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    throw new NotFoundException("User not found");
+  }
+
+  const isPasswordValid = await user.comparePassword(password);
+  if (!isPasswordValid) {
+    throw new UnauthorizedException("Invalid credentials");
+  }
+
+  const { token, expiresAt } = signJwtToken({
+    userId: user.id,
+  });
+
+  const reportSetting = await ReportSettingModel.findOne(
+    {
+      userId: user.id,
+    },
+    {
+      _id: 1,
+      frequency: 1,
+      isEnabled: 1,
+    }
+  ).lean();
+
+  return {
+    user: user.omitPassword(),
+    accessToken: token,
+    expiresAt,
+    reportSetting,
+  };
 };
