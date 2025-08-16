a15eb66b2606f9efc5691db0fed2ba239da3000d
ifaamrillah <amrillah.work@gmail.com>
2025-08-16 18:03:15 +0700

    feat: add format zod error

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,11 +1,31 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
+import { ZodError } from "zod";
 
 import { HTTP_STATUS } from "../configs/http.config";
 
+import { ErrorCodeEnum } from "../enums/error-code.enum";
+
 import { AppError } from "../utils/app-error";
 
+const formatZodError = (err: ZodError, res: Response) => {
+  const errors = err?.issues?.map((error) => ({
+    field: error.path.join("."),
+    message: error.message,
+  }));
+
+  return res.status(HTTP_STATUS.BAD_REQUEST).json({
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+    message: "Validation Error",
+    errors,
+  });
+};
+
 export const errorHandler: ErrorRequestHandler = (err, req, res, next): any => {
-  console.log("Error occurred on PATH:", req.path);
+  console.log("Error occurred on PATH:", req.path, err);
+
+  if (err instanceof ZodError) {
+    return formatZodError(err, res);
+  }
 
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
