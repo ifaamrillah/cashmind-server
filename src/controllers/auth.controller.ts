b793f24e1296d694c65f72f41aca7a7aed74c767
ifaamrillah <amrillah.work@gmail.com>
2025-08-16 18:26:42 +0700

    feat: add login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,9 +4,9 @@ import { HTTP_STATUS } from "../configs/http.config";
 
 import { asyncHandler } from "../middlewares/async-handler.middleware";
 
-import { registerService } from "../services/auth.service";
+import { loginService, registerService } from "../services/auth.service";
 
-import { registerSchema } from "../validators/auth.validator";
+import { loginSchema, registerSchema } from "../validators/auth.validator";
 
 export const registerController = asyncHandler(
   async (req: Request, res: Response) => {
@@ -16,6 +16,19 @@ export const registerController = asyncHandler(
 
     res.status(HTTP_STATUS.CREATED).json({
       message: "User registered successfully",
+      data: result,
+    });
+  }
+);
+
+export const loginController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const body = loginSchema.parse(req.body);
+
+    const result = await loginService(body);
+
+    res.status(HTTP_STATUS.OK).json({
+      message: "User logged in successfully",
       data: result,
     });
   }
