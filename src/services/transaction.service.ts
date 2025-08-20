bc9bef1c0a2382b2f596d7892e4e0b49b0f577fa
ifaamrillah <amrillah.work@gmail.com>
2025-08-20 10:54:00 +0700

    feat: add get all transaction service

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,4 +1,6 @@
-import TransactionModel from "../models/transaction.model";
+import TransactionModel, {
+  TransactionTypeEnum,
+} from "../models/transaction.model";
 
 import { CreateTransactionSchema } from "../validators/transaction.validator";
 
@@ -35,4 +37,74 @@ export const createTransactionService = async (
   });
 
   return transaction;
+};
+
+export const getAllTransactionService = async (
+  userId: string,
+  filters: {
+    keyword?: string;
+    type?: keyof typeof TransactionTypeEnum;
+    recurringStatus?: "RECURRING" | "NON_RECURRING";
+  },
+  pagination: {
+    pageSize: number;
+    pageNumber: number;
+  }
+) => {
+  const { keyword, type, recurringStatus } = filters;
+
+  const filterConditions: Record<string, any> = {
+    userId,
+  };
+
+  if (keyword) {
+    filterConditions.$or = [
+      {
+        title: {
+          $regex: keyword,
+          $options: "i",
+        },
+        category: {
+          $regex: keyword,
+          $options: "i",
+        },
+      },
+    ];
+  }
+
+  if (type) {
+    filterConditions.type = type;
+  }
+
+  if (recurringStatus) {
+    if (recurringStatus === "RECURRING") {
+      filterConditions.isRecurring = true;
+    } else if (recurringStatus === "NON_RECURRING") {
+      filterConditions.isRecurring = false;
+    }
+  }
+
+  const { pageSize, pageNumber } = pagination;
+  const skip = (pageNumber - 1) * pageSize;
+
+  const [transactions, totalCount] = await Promise.all([
+    TransactionModel.find(filterConditions)
+      .skip(skip)
+      .limit(pageSize)
+      .sort({ createdAt: -1 }),
+    TransactionModel.countDocuments(filterConditions),
+  ]);
+
+  const totalPages = Math.ceil(totalCount / pageSize);
+
+  return {
+    transactions,
+    pagination: {
+      pageSize,
+      pageNumber,
+      totalCount,
+      totalPages,
+      skip,
+    },
+  };
 };
