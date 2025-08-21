b75b8dee1bc6b9a87bca40d010533230e8b88b7b
ifaamrillah <amrillah.work@gmail.com>
2025-08-21 10:06:35 +0700

    feat: add get transaction by id route

--- src/routes/transaction.route.ts
+++ src/routes/transaction.route.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import {
   createTransactionController,
   getAllTransactionController,
+  getTransactionByIdController,
 } from "../controllers/transaction.controller";
 
 const transactionRoutes = Router();
 
-transactionRoutes.get("/", getAllTransactionController);
 transactionRoutes.post("/", createTransactionController);
+transactionRoutes.get("/", getAllTransactionController);
+transactionRoutes.get("/:id", getTransactionByIdController);
 
 export default transactionRoutes;
